6a96a874538e6b8b9d9239fc8bc0a6295d34c027
RicardoMuchacho <ricardojrmuchacho@gmail.com>
2025-02-19 21:02:12 +0100

    feat: particles background

---
+++ src/hooks/useMobile.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+const useMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    // Set initial value
+    handleResize();
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Clean up event listener on unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return isMobile;
+};
+
+export default useMobile;
