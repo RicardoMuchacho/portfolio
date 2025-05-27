8f95e9eca68748c1ba95b43349cb2964b88fc762
RicardoMuchacho <ricardojrmuchacho@gmail.com>
2025-05-27 16:44:24 +0200

    web3 portfolio

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,42 +2,42 @@ import Image from "next/image";
 import ParticlesBg from "./ui/ParticlesBg";
 
 export default function Hero() {
-  return (
-    <section className="relative flex flex-col mt-16 gap-2 items-center justify-center py-24 px-5 w-full text-base border-b-2 border-gray-200 sm:flex-row sm:gap-10 sm:px-20 sm:py-24">
-      <ParticlesBg />
-      {/* Profile Image */}
-      <Image
-        src="/profileImg.png"
-        alt="Profile"
-        width={215}
-        height={215}
-        className="object-contain shadow-lg rounded-full z-0 border-gray-100 border-2"
-      />
-      <div className="relative flex items-center w-full h-full bg-white drop-shadow-lg bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-lg border border-white border-opacity-20 max-w-[90%] md:max-w-[650px]">
-        {/* <Image
+    return (
+        <section className="relative flex flex-col mt-16 gap-2 items-center justify-center py-24 px-5 w-full text-base border-b-2 border-gray-200 sm:flex-row sm:gap-10 sm:px-20 sm:py-24">
+            <ParticlesBg />
+            {/* Profile Image */}
+            <Image
+                src="/profileImg.png"
+                alt="Profile"
+                width={215}
+                height={215}
+                className="object-contain shadow-lg rounded-full z-0 border-gray-100 border-2"
+            />
+            <div className="relative flex items-center w-full h-full bg-white drop-shadow-lg bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-lg border border-white border-opacity-20 max-w-[90%] md:max-w-[650px]">
+                {/* <Image
             src="/message.svg"
             alt="Message image"
             width={700}
             height={300}
             className="w-full h-auto rounded-lg"
           /> */}
-        {/* <Image
+                {/* <Image
           src="/messageMobile.svg"
           alt="Message image mobile"
           width={300}
           height={180}
           className="object-cover absolute w-full h-[180px] max-w-full drop-shadow-lg sm:hidden"
           /> */}
-        <div className="relative mt-4 flex flex-col px-4 py-4  md:px-4 md:pl-8 w-full sm:mt-0 max-w-full">
-          <h1 className="text-lg font-bold mb-2 text-center sm:text-4xl sm:mb-4 sm:text-start">
-            Hello! I'm Ricardo
-          </h1>
-          <p className="text-sm text-black sm:text-lg">
-            A frontend developer with a passion for problem-solving and creating
-            efficient, user-friendly web applications.
-          </p>
-        </div>
-      </div>
-    </section>
-  );
+                <div className="relative mt-4 flex flex-col px-4 py-4  md:px-4 md:pl-8 w-full sm:mt-0 max-w-full">
+                    <h1 className="text-lg font-bold mb-2 text-center sm:text-4xl sm:mb-4 sm:text-start">
+                        Hello! I'm Ricardo
+                    </h1>
+                    <p className="text-sm text-black sm:text-lg">
+                        A fullstack web3 developer with a passion for problem-solving and creating
+                        efficient, user-friendly decentralized applications. Check some of my recents projects below.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
 }
--- src/components/ui/ParticlesBg.tsx
+++ src/components/ui/ParticlesBg.tsx
@@ -5,125 +5,125 @@ import { loadSlim } from "@tsparticles/slim";
 import useMobile from "@/hooks/useMobile";
 
 const ParticlesBg = () => {
-  const [init, setInit] = useState(false);
-  const isMobile = useMobile();
+    const [init, setInit] = useState(false);
+    const isMobile = useMobile();
 
-  useEffect(() => {
-    initParticlesEngine(async (engine) => {
-      await loadSlim(engine);
-    }).then(() => {
-      setInit(true);
-    });
-  }, []);
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            await loadSlim(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
 
-  return (
-    init && (
-      <div className="absolute top-0 left-0 right-0 h-full -z-5">
-        <Particles
-          id="tsparticles"
-          className="w-full h-full"
-          options={{
-            fullScreen: { enable: false },
-            background: {
-              color: {
-                value: "#FFFFFF",
-              },
-            },
-            fpsLimit: 120,
-            interactivity: {
-              events: {
-                onClick: {
-                  enable: true,
-                  mode: "push",
-                },
-                onHover: {
-                  enable: true,
-                  mode: "repulse",
-                },
-                // resize: true,
-              },
-              modes: {
-                push: {
-                  quantity: 4,
-                },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4,
-                },
-              },
-            },
-            particles: {
-              move: {
-                enable: true,
-                speed: { min: 1, max: 6 }
-              },
-              number: {
-                value: isMobile ? 8 : 20,
-                limit: { value: isMobile ? 8 : 20, mode: "wait" },
-              },
-              opacity: {
-                value: 1
-              },
-              rotate: {
-                path: true
-              },
-              shape: {
-                options: {
-                  image: [
-                    {
-                      height: 200,
-                      src: "/bgIcons/html.svg",
-                      width: 200
-                    },
-                    {
-                      height: 200,
-                      src: "/bgIcons/css.svg",
-                      width: 200
-                    },
-                    {
-                      height: 200,
-                      src: "/bgIcons/jest.svg",
-                      width: 200
-                    },
-                    {
-                      height: 200,
-                      src: "/bgIcons/js.svg",
-                      width: 200
-                    },
-                    {
-                      height: 200,
-                      src: "/bgIcons/react.svg",
-                      width: 200
-                    },
-                    {
-                      height: 200,
-                      src: "/bgIcons/node.svg",
-                      width: 200
-                    },
-                    {
-                      gif: true,
-                      height: 200,
-                      src: "/bgIcons/tailwind.svg",
-                      width: 200
-                    },
-                  ]
-                },
-                type: "image"
-              },
-              size: {
-                value: {
-                  min: 32,
-                  max: 64
-                }
-              }
-            },
-            detectRetina: true,
-          }}
-        />
-      </div>
+    return (
+        init && (
+            <div className="absolute top-0 left-0 right-0 h-full -z-5">
+                <Particles
+                    id="tsparticles"
+                    className="w-full h-full"
+                    options={{
+                        fullScreen: { enable: false },
+                        background: {
+                            color: {
+                                value: "#FFFFFF",
+                            },
+                        },
+                        fpsLimit: 120,
+                        interactivity: {
+                            events: {
+                                onClick: {
+                                    enable: true,
+                                    mode: "push",
+                                },
+                                onHover: {
+                                    enable: true,
+                                    mode: "repulse",
+                                },
+                                // resize: true,
+                            },
+                            modes: {
+                                push: {
+                                    quantity: 4,
+                                },
+                                repulse: {
+                                    distance: 200,
+                                    duration: 0.4,
+                                },
+                            },
+                        },
+                        particles: {
+                            move: {
+                                enable: true,
+                                speed: { min: 1, max: 6 }
+                            },
+                            number: {
+                                value: isMobile ? 8 : 20,
+                                limit: { value: isMobile ? 8 : 20, mode: "wait" },
+                            },
+                            opacity: {
+                                value: 1
+                            },
+                            rotate: {
+                                path: true
+                            },
+                            shape: {
+                                options: {
+                                    image: [
+                                        {
+                                            height: 200,
+                                            src: "/bgCrypto/bitcoin.svg",
+                                            width: 200
+                                        },
+                                        {
+                                            height: 150,
+                                            src: "/bgCrypto/ethereum.svg",
+                                            width: 100
+                                        },
+                                        {
+                                            height: 200,
+                                            src: "/bgCrypto/arbitrum.svg",
+                                            width: 200
+                                        },
+                                        {
+                                            height: 200,
+                                            src: "/bgCrypto/chainlink.svg",
+                                            width: 200
+                                        },
+                                        {
+                                            height: 200,
+                                            src: "/bgCrypto/uniswap.svg",
+                                            width: 200
+                                        },
+                                        {
+                                            height: 200,
+                                            src: "/bgCrypto/foundry.svg",
+                                            width: 200
+                                        },
+                                        {
+                                            gif: true,
+                                            height: 200,
+                                            src: "/bgCrypto/tailwind.svg",
+                                            width: 200
+                                        },
+                                    ]
+                                },
+                                type: "image"
+                            },
+                            size: {
+                                value: {
+                                    min: 32,
+                                    max: 64
+                                }
+                            }
+                        },
+                        detectRetina: true,
+                    }}
+                />
+            </div>
 
-    )
-  );
+        )
+    );
 };
 
 export default ParticlesBg
