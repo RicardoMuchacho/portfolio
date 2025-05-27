34aebd370466965a420fab6d05e5d6fd1efc2070
RicardoMuchacho <ricardojrmuchacho@gmail.com>
2025-05-27 17:01:56 +0200

    web3 projects update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,38 +1,38 @@
 import ContentCard, { IContentCard } from "./ui/ContentCard";
 
 const projects: IContentCard[] = [
-  {
-    title: "Restaurantly App",
-    description: "Google Api recommendations based on text string",
-    url: "https://github.com/RicardoMuchacho/restaurantly",
-    src: "/restaurantlyImg.png"
-  },
-  {
-    title: "WebGPU Background Remover",
-    description: "WebGPU and hugginface model to remove background for portrait images",
-    url: "https://github.com/RicardoMuchacho/webgpu-playground",
-    src: "/webGpuImg.png"
-  },
-  {
-    title: "University Projects",
-    description: "Compilation of projects developed during my time at university",
-    url: "https://github.com/stars/RicardoMuchacho/lists/web-app-projects",
-    src: "/githubBg.jpg"
-  },
+    {
+        title: "Arbiswap Dapp",
+        description: "Decentralized Exchange (DEX) on arbitrum",
+        url: "https://arbiswap-lovat.vercel.app/",
+        src: "/arbiswap.png"
+    },
+    {
+        title: "NFT Marketplace",
+        description: "Web3 NFT Marketplace Dapp on arbitrum sepolia",
+        url: "https://nft-marketplace-front-chi.vercel.app/",
+        src: "/nftMarketplace.png"
+    },
+    {
+        title: "Other Projects",
+        description: "Compilation of personal projects",
+        url: "https://github.com/stars/RicardoMuchacho",
+        src: "/githubBg.jpg"
+    },
 ]
 
 export default function Projects() {
-  return (
-    <div className="flex flex-col gap-10 justify-center items-center w-full sm:flex-row">
-      {projects.map((project) => (
-        <ContentCard
-          key={project.title}
-          title={project.title}
-          description={project.description}
-          src={project.src}
-          url={project.url}
-        />
-      ))}
-    </div>
-  );
+    return (
+        <div className="flex flex-col gap-10 justify-center items-center w-full sm:flex-row">
+            {projects.map((project) => (
+                <ContentCard
+                    key={project.title}
+                    title={project.title}
+                    description={project.description}
+                    src={project.src}
+                    url={project.url}
+                />
+            ))}
+        </div>
+    );
 }
